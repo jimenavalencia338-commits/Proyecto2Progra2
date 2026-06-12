522c20d17ddaec621fe9eca79ba558d8f47b265b
Chris <chrissanchez2105@gmail.com>
2026-06-12 01:51:18 -0600

    Fix include path for GameEvent header file

--- include/events/GameEvent.h
+++ include/events/GameEvent.h
@@ -21,3 +21,4 @@ public:
 
     bool isActivated() const;
 };
+
--- src/events/GameEvent.cpp
+++ src/events/GameEvent.cpp
@@ -1,4 +1,4 @@
-#include "../../include/events/GameEvent.h"
+#include "events/GameEvent.h"
 
 GameEvent::GameEvent(const string& description)
     : description(description), activated(false) {
